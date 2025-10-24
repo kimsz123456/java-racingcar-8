4cdaacf72a6fd0fdc0e9e1cc7f59b88374cd7feb
kimsz123456 <kimsz123456@gmail.com>
2025-10-24 18:25:10 +0900

    feat: 출력 기능 추가

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,5 +1,31 @@
 package racingcar;
 
+import java.util.List;
+
 public class OutputView {
-    
+    private static final String CAR_NAME_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String COUNT_PROMPT = "시도할 횟수는 몇 회인가요?";
+    private static final String RACE_RESULT = "실행 결과";
+    private static final String WINNER_ANNOUNCEMENT = "최종 우승자 : ";
+
+    public void printCarNamePrompt() {
+        System.out.println(CAR_NAME_PROMPT);
+    }
+
+    public void printCountPrompt() {
+        System.out.println(COUNT_PROMPT);
+    }
+
+    public void printRaceResultPrompt(List<Car> carList) {
+        System.out.println(RACE_RESULT);
+        for (Car car : carList) {
+            car.printPosition();
+        }
+        System.out.println();
+    }
+
+    public void printWinnerAnouncementPrompt(List<String> winners) {
+        String winnerNames = String.join(", ", winners);
+        System.out.printf((WINNER_ANNOUNCEMENT) + "%n", winnerNames);
+    }
 }
