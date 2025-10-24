da081911cc205cab0fd8c1e6cc94b13377f5d28b
kimsz123456 <kimsz123456@gmail.com>
2025-10-24 17:53:14 +0900

    feat: 입력 기능 추가

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String readCarName() {
+        /*
+        자동차 이름 입력
+        입력 검증 로직 추가 예정
+         */
+        return Console.readLine();
+    }
+    public String readCount() {
+        /*
+        시도 횟수 입력
+        입력 검증 로직 추가 예정
+         */
+        return Console.readLine();
+    }
+}
