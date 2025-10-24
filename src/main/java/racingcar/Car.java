716673d773afb9699c51fc1feaea38d742c7e098
kimsz123456 <kimsz123456@gmail.com>
2025-10-24 18:17:28 +0900

    feat: 자동차 클래스 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,36 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void tryMove() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        if (randomNumber >= 4) {
+            position++;
+        }
+    }
+
+    public void printPosition() {
+        System.out.print(name + " : ");
+        for (int i = 0; i < position; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
 }
